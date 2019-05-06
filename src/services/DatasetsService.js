d827f694c016ad1f68fb4d7bdce1a23f546b5810
Mateusz Werpachowski <matyszxzx@gmail.com>
2019-05-06 23:02:11 +0200

    -

--- src/services/DatasetsService.js
+++ src/services/DatasetsService.js
@@ -1,6 +1,8 @@
+import axios from 'axios'
+
 export default {
-  population: import('../datasets/population.json'),
-  countryCodes: import('../datasets/country-codes.json'),
-  worldCities: import('../datasets/world-cities.json'),
-  geo: import('../datasets/geo.json'),
+  population: axios.get('https://pkgstore.datahub.io/core/population/population_json/data/43d34c2353cbd16a0aa8cadfb193af05/population_json.json').then(res => res.data),
+  countryCodes: axios.get('https://pkgstore.datahub.io/core/country-codes/country-codes_json/data/471a2e653140ecdd7243cdcacfd66608/country-codes_json.json').then(res => res.data),
+  worldCities: axios.get('https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json').then(res => res.data),
+  geo: axios.get('https://s3.amazonaws.com/rawstore.datahub.io/23f420f929e0e09c39d916b8aaa166fb.geojson').then(res => res.data),
 }
